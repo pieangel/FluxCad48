471f795dc36f6b9e29157a69feff6ec72b475c48
pieangel <pieangel@naver.com>
2026-05-21 09:05:06 +0900

    Initial FluxCad48 project setup

---
+++ FluxCad48/Commands.cs
@@ -0,0 +1,18 @@
+﻿using Bricscad.ApplicationServices;
+using Bricscad.EditorInput;
+using Teigha.Runtime;
+
+namespace FluxCad48
+{
+	public class Commands
+	{
+		[CommandMethod("FLUX_HELLO")]
+		public void FluxHello()
+		{
+			Document doc = Application.DocumentManager.MdiActiveDocument;
+			Editor ed = doc.Editor;
+
+			ed.WriteMessage("\n[FluxCad48] Hello BricsCAD .NET Framework 4.8 Plugin!");
+		}
+	}
+}
