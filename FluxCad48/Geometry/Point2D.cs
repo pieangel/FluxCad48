800ee9a6b3c746ab366cd45d2a2c2e7ca8876d55
pieangel <pieangel@naver.com>
2026-05-22 11:39:51 +0900

    1. 쉬트 판정을 위한 클래스 추가.

---
+++ FluxCad48/Geometry/Point2D.cs
@@ -0,0 +1,19 @@
+﻿namespace FluxCad48.Geometry
+{
+	public readonly struct Point2D
+	{
+		public double X { get; }
+		public double Y { get; }
+
+		public Point2D(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public override string ToString()
+		{
+			return $"({X:0.###}, {Y:0.###})";
+		}
+	}
+}
---
+++ FluxCad48/Sheets/SelectedSheetUnitInfo.cs
@@ -0,0 +1,29 @@
+﻿using Teigha.DatabaseServices;
+using FluxCad48.Geometry;
+
+namespace FluxCad48.Sheets
+{
+	public sealed class SelectedSheetUnitInfo
+	{
+		public ObjectId Id { get; set; }
+
+		public string HandleText { get; set; } = "";
+		public string TypeName { get; set; } = "";
+		public string Layer { get; set; } = "";
+
+		public string BlockName { get; set; } = "";
+
+		public Bounds2D BoundsWcs { get; set; }
+		public Point2D CenterWcs { get; set; }
+
+		public string Text { get; set; } = "";
+		public string NormalizedText { get; set; } = "";
+
+		public bool IsTextLike { get; set; }
+		public bool IsDimensionLike { get; set; }
+		public bool IsCenterLineLike { get; set; }
+		public bool IsHiddenLineLike { get; set; }
+		public bool IsBlockReference { get; set; }
+		public bool IsLineLike { get; set; }
+	}
+}
